7d6a2e8075751017948c2c95acf0808bebad9a62
Lawrence Hunt <huntlawrence@gmail.com>
2025-04-10 07:25:10 +0700

    add jest setup file with global imports

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json'
-    }]
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
   },
   moduleNameMapper: {
-    '^macrocosmos$': '<rootDir>/src/index.ts',
-    '^macrocosmos/(.*)$': '<rootDir>/src/$1'
-  }
-}; 
+    "^macrocosmos$": "<rootDir>/src/index.ts",
+    "^macrocosmos/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFiles: ["<rootDir>/src/__tests__/jest.setup.ts"],
+};
