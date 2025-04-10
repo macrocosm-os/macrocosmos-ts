9e6c2cbcb6cbae11792c8d879ff341646a58fb6a
Lawrence Hunt <huntlawrence@gmail.com>
2025-04-10 10:32:50 +0700

    fix ts error in constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,10 @@ export const CLIENT_NAME = "macrocosmos-ts-sdk";
 
 // Package version from package.json
 const packageJsonPath = path.resolve(__dirname, "../package.json");
-export const VERSION = JSON.parse(
-  fs.readFileSync(packageJsonPath, "utf8"),
-).version;
+const packageJsonContent = fs.readFileSync(packageJsonPath, "utf8");
+const packageJson = JSON.parse(packageJsonContent) as { version: string };
+export const VERSION = packageJson.version;
+
 export const BASE_URL =
   process.env.MACROCOSMOS_BASE_URL || "constellation.api.cloud.macrocosmos.ai";
 export const APEX_API_KEY =
