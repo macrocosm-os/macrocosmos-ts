33e57a841eace9d144f1c59d63b70f77ae23303d
Lawrence Hunt <huntlawrence@gmail.com>
2025-05-05 14:16:46 +0700

    add dotenv to jest setup file so we can add an API key for running tests locally

--- src/__tests__/jest.setup.ts
+++ src/__tests__/jest.setup.ts
@@ -1,8 +1,4 @@
 import "@jest/globals";
 import { config } from "dotenv";
-
 // Load environment variables from .env file
 config();
-
-console.log("Environment variables loaded from .env file");
-console.log("api key:", process.env.MACROCOSMOS_API_KEY);
