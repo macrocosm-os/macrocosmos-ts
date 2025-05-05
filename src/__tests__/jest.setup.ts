4fcddfdefaf4667a22e9ef174a5e389e84e5fd19
Lawrence Hunt <huntlawrence@gmail.com>
2025-05-05 13:10:34 +0700

    add dotenv and run it in test setup

--- src/__tests__/jest.setup.ts
+++ src/__tests__/jest.setup.ts
@@ -1,1 +1,8 @@
 import "@jest/globals";
+import { config } from "dotenv";
+
+// Load environment variables from .env file
+config();
+
+console.log("Environment variables loaded from .env file");
+console.log("api key:", process.env.MACROCOSMOS_API_KEY);
