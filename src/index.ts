19f2bf76531036219a63239e683f87cb6139d4db
Lawrence Hunt <huntlawrence@gmail.com>
2025-05-05 12:58:12 +0700

    export generated types

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ export * from "./constants";
 
 // Export apex modules
 export * from "./lib/apex";
+export * from "./generated/apex/v1/apex";
 
 // Export gravity modules
 export * from "./lib/gravity";
+export * from "./generated/gravity/v1/gravity";
 
 // Export billing modules
 export * from "./lib/billing";
+export * from "./generated/billing/v1/billing";
 
 // Export sn13 modules
 export * from "./lib/sn13";
+export * from "./generated/sn13/v1/sn13_validator";
