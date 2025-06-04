430117ec5bf64a9afc44ac8b2e4f0fbf69bb9b48
Lawrence Hunt <huntlawrence@gmail.com>
2025-06-04 16:51:39 +0700

    export some additional types from apex client

--- src/lib/apex/Client.ts
+++ src/lib/apex/Client.ts
@@ -25,9 +25,13 @@ type WebRetrievalRequest = MarkFieldsOptional<
   "uids"
 >;
 
+type ApexStreamResponse = ApexStream<ChatCompletionChunkResponse>;
+
 // re-export the types for use in the package
 export {
   ApexStream,
+  ChatCompletionChunkResponse,
+  ApexStreamResponse,
   WebRetrievalRequest,
   WebRetrievalResponse,
   ChatCompletionRequest,
@@ -56,7 +60,7 @@ export interface ChatCompletionsCreate {
     params: ChatCompletionRequest & { stream: true },
     /** options are not used, but are accepted for compatibility with the OpenAI API */
     _options?: unknown,
-  ): Promise<ApexStream<ChatCompletionChunkResponse>>;
+  ): Promise<ApexStreamResponse>;
   (
     params: ChatCompletionRequest & { stream?: false | undefined },
     /** options are not used, but are accepted for compatibility with the OpenAI API */
@@ -69,7 +73,7 @@ function chatCompletionsCreate(
   params: ChatCompletionRequest & { stream: true },
   /** options are not used, but are accepted for compatibility with the OpenAI API */
   _options?: unknown,
-): Promise<ApexStream<ChatCompletionChunkResponse>>;
+): Promise<ApexStreamResponse>;
 function chatCompletionsCreate(
   this: ApexClient,
   params: ChatCompletionRequest & { stream?: false | undefined },
@@ -79,7 +83,7 @@ function chatCompletionsCreate(
   this: ApexClient,
   params: ChatCompletionRequest,
   _options?: unknown,
-): Promise<ApexStream<ChatCompletionChunkResponse> | ChatCompletionResponse> {
+): Promise<ApexStreamResponse | ChatCompletionResponse> {
   const client = this.createGrpcClient();
   const requestParams = {
     ...params,
