db0bcba894adb3b0616448fd2d308fef59d38e7c
Lawrence Hunt <huntlawrence@gmail.com>
2025-11-06 15:46:44 +0700

    remove apex from constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,6 @@ export const CLIENT_NAME = "macrocosmos-ts-sdk";
 
 export const BASE_URL =
   process.env.MACROCOSMOS_BASE_URL || "constellation.api.cloud.macrocosmos.ai";
-export const APEX_API_KEY =
-  process.env.APEX_API_KEY || process.env.MACROCOSMOS_API_KEY;
+
 export const GRAVITY_API_KEY =
   process.env.GRAVITY_API_KEY || process.env.MACROCOSMOS_API_KEY;
