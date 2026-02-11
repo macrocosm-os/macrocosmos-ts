a9299963c5b3d2d188b63f8ea29659b9d75b0e78
gevagorou-mai <giannis.evagorou@macrocosmos.ai>
2026-02-11 12:09:23 +0200

    GEN-2756: typescript clients (#56)
    * Squashed 'protos/' changes from efc3abf..c002adc
    
    c002adc GEN-2662: Add protos for building datasets periodically and for upserting/selecting gravity_pre_built_user_datasets
    a8377dc GEN-2530: Remove persistent pipelines (#41)
    
    git-subtree-dir: protos
    git-subtree-split: c002adcf867fe019866380015c633e0655499420
    
    * Squashed 'protos/' changes from c002adc..fae66a2
    
    fae66a2 GEN-2672: Add pre build dataset countS (#43)
    
    git-subtree-dir: protos
    git-subtree-split: fae66a280a29a1b0c2dcf6fb86675030c43a73bb
    
    * Add typescript clients for the remaining gravity endpoints
    
    * Add typescript clients for the remaining gravity endpoints
    
    * Prettify

--- src/__tests__/sn13/client.test.ts
+++ src/__tests__/sn13/client.test.ts
@@ -73,7 +73,7 @@ describe("Sn13Client", () => {
 
     // Log response for debugging
     console.log("On-Demand Data Response:", response);
-  }, 60000); // Increase timeout to 60 seconds
+  }, 160000); // Increase timeout to 60 seconds
 
   it("should fetch on-demand data with keyword mode", async () => {
     // Create Sn13Client
@@ -103,7 +103,7 @@ describe("Sn13Client", () => {
 
     // Log response for debugging
     console.log("On-Demand Data Response:", response);
-  }, 60000); // Increase timeout to 60 seconds
+  }, 160000); // Increase timeout to 60 seconds
 
   it("should fetch on-demand data with URL mode", async () => {
     // Create Sn13Client
@@ -129,5 +129,5 @@ describe("Sn13Client", () => {
 
     // Log response for debugging
     console.log("On-Demand Data Response:", response);
-  }, 200000); // Increase timeout to 60 seconds
+  }, 1200000); // Increase timeout to 60 seconds
 });
